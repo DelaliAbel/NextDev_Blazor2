6952848fb91109877ef5703637f953c7bee87c6d
Delali Abel <delaliabel@gmail.com>
2025-12-20 00:06:55 +0100

    Add project files.

---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,12 @@
+namespace NextDev_Blazor2.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}

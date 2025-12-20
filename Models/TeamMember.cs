97d6e8dc3a1c17cbf44ad140c3c3c159b98142b5
Delali Abel <delaliabel@gmail.com>
2025-12-20 02:49:59 +0100

    Modification Dynamique

---
+++ Models/TeamMember.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace NextDev_Blazor2.Models
+{
+    public class TeamMember
+    {
+        [Key]
+        public int? MemberID { get; set; }
+        public string? MemberFullName { get; set; }
+        public string? MemberTitle { get; set; }
+        public string? MemberDescription { get; set; }
+        public string? MemberPhone { get; set; }
+        public string? MemberEmail { get; set; }
+        public string? MemberAddress { get; set; }
+        public string? MemberImage { get; set; }
+    }
+}
